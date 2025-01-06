1841cfe0ef5e2c7f1d31a0ba8b361460a8bad653
Yerko Opazo <yerk.opazo.h@gmail.com>
2025-01-06 19:01:45 +0100

    Add files via upload

---
+++ Pokedex/script.js
@@ -0,0 +1,6 @@
+var testButton = document.getElementById("myButton");
+
+testButton.addEventListener("mouseover, function (){
+    var list = testButton.classList; 
+    list.add(mouseOverClass)")
+    
